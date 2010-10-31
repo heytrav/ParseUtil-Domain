26485afb3a8da469992ee5863281ee106c379f90
William Travis Holton <wtholton@googlemail.com>
2010-10-31 20:31:12 +0100

    puny encode segments

--- lib/ParseUtil/Domain.pm
+++ lib/ParseUtil/Domain.pm
@@ -9,35 +9,56 @@ use ParseUtil::Domain::ConfigData;
 use Net::IDN::Encode ':all';
 use Net::IDN::Punycode ':all';
 use Smart::Comments;
+use YAML;
 use utf8;
 
 sub parse_domain : Export(:DEFAULT) {    #{{{
     my $name = shift;
     my @name_segments = split /\@/, $name;
+    ### namesegments : Dump(\@name_segments)
 
     my @segments = split /[\.\x{FF0E}\x{3002}\x{FF61}]/, $name_segments[-1];
     ### executing with : $name
-    my $zone = _find_zone( \@segments );
+    my ( $zone,$zone_ace, $domain_segments ) =
+      @{ _find_zone( \@segments ) }{qw/zone zone_ace domain/};
     ### found zone : $zone
-    my $puny_processed = _punycode_segments( \@segments, $zone );
+    ### found zone_ace : $zone_ace
+
+    my $puny_processed = _punycode_segments( $domain_segments, $zone );
+    ### puny_processed : Dump($puny_processed)
 
 }    #}}}
 
 sub _find_zone {    #{{{
     my $domain_segments = shift;
-    my $zone;
-    my $tld_regex = ParseUtil::Domain::ConfigData->config('tld_regex');
-    my $tld       = pop @{$domain_segments};
-    my $sld       = pop @{$domain_segments};
+    my $tld_regex       = ParseUtil::Domain::ConfigData->config('tld_regex');
+    my $tld             = pop @{$domain_segments};
+    my $sld             = pop @{$domain_segments};
 
-    my $possible_tld = join "." => map { domain_to_ascii($_) } $sld,
-      $tld;
+    my $possible_tld = join "." => map { domain_to_ascii($_) } $tld, $sld;
+    my @zone_params;
     if ( $possible_tld =~ /\A$tld_regex\z/ ) {
-        return { zone => $possible_tld, domain => $domain_segments };
+        my $zone = join "." => $sld, $tld;
+        my $zone_ace = join "." => map { domain_to_ascii($_)} $sld, $tld;
+        push @zone_params, zone_ace => $zone_ace
+          unless $zone eq $zone_ace;
+        return {
+            zone   => $zone,
+            domain => $domain_segments,
+            @zone_params,
+        };
     }
-    if ( $tld =~ /\A$tld_regex\z/ ) {
+    my $zone_ace = domain_to_ascii($tld);
+    if ( $zone_ace =~ /\A$tld_regex\z/ ) {
         push @{$domain_segments}, $sld;
-        return { zone => $tld, domain => $domain_segments };
+        push @zone_params, zone_ace => $zone_ace
+          unless $zone_ace eq $tld;
+
+        return {
+            zone     => $tld,
+            domain   => $domain_segments,
+            @zone_params
+        };
     }
     die "Could not find tld.";
 }    #}}}
@@ -45,7 +66,19 @@ sub _find_zone {    #{{{
 sub _punycode_segments {    #{{{
     my ( $domain_segments, $zone ) = @_;
 
+    if ($zone !~ /^de$/) {
+        my $puny_encoded = [map { domain_to_ascii($_)  } @{$domain_segments} ];
+        my $puny_decoded = [ map { domain_to_unicode($_)  } @{$puny_encoded}
+        ];
+        return { domain => $puny_decoded, domain_ace => $puny_encoded};
+    }
+   
+    my @sub_param;
+
 }    #}}}
+
+sub _non_normative_punyencode { #{{{
+} #}}}
 
 "one, but we're not the same";
 
