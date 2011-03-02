93a0f4d37cf9c99623a3ec02017daa05cebb5230
William Travis Holton <holton@united-domains.de>
2011-03-02 10:42:27 +0100

    raising version for development

--- lib/ParseUtil/Domain.pm
+++ lib/ParseUtil/Domain.pm
@@ -3,7 +3,7 @@ package ParseUtil::Domain;
 use strict;
 use warnings;
 
-use version 0.77; our $VERSION = qv("v1.2.2");
+use version 0.77; our $VERSION = qv("v1.2.3");
 use Perl6::Export::Attrs;
 use ParseUtil::Domain::ConfigData;
 use Net::IDN::Encode ':all';
